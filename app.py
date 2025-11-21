1d275fd49576ac990ffa7785dc72c6cc9cd5b9e6
Awtuh <motionplaylove@gmail.com>
2025-11-21 15:43:04 +0100

    création du app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portfolio Finance", page_icon="📈")
+st.title("Portfolio Mini-projets Finance")
+st.info("La plateforme Streamlit arrive. Restez connectés pour découvrir un nouveau mini-projet chaque semaine !")
